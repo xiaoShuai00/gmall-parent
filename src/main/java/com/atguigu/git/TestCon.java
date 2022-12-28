1dbe1d888edeec7f2fc07ff74a2a8c9449049ae8
XiaoShuai <XiaoShuai@guigu.com>
2022-12-28 16:48:41 +0800

    hot-fix second merge commit

--- src/main/java/com/atguigu/git/TestCon.java
+++ src/main/java/com/atguigu/git/TestCon.java
@@ -8,5 +8,6 @@ public class TestCon {
         System.out.println("helloWorld!第二次提交添加的代码");
         System.out.println("helloWorld!第三次提交添加的代码");
         System.out.println("hot-fix  第一次提交的代码");
+        System.out.println("hot-fix  第二次提交的代码  合并代码冲突演示");
     }
 }
