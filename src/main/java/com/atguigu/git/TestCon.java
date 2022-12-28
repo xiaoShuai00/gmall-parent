e35c3b54e6cfc9df7a0ebe429b730a35dfb004b7
XiaoShuai <XiaoShuai@guigu.com>
2022-12-28 16:09:51 +0800

    master first commit

---
+++ src/main/java/com/atguigu/git/TestCon.java
@@ -0,0 +1,9 @@
+package com.atguigu.git;
+
+public class TestCon {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+        System.out.println("helloWorld");
+        System.out.println("helloWorld");
+    }
+}
